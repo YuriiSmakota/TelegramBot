05a60f7de311df7ff3fc582c61f91936fdababda
YuriiSmakota <yurii.smakota@gmail.com>
2023-10-21 17:03:06 +0300

    AddSettingsFile

--- bot.py
+++ bot.py
@@ -1,11 +1,17 @@
 import asyncio
+import logging
+
 from aiogram import Bot, Dispatcher
+from settings import config
 
 
 async def main():
-    bot = Bot(token="TOKEN")
+    logging.basicConfig(level=logging.INFO)
+
+    bot = Bot(token=config.bot_token.get_secret_value(), parse_mode="HTML")
     dp = Dispatcher()
 
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
