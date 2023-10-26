487a14e3d7f3b9c62085f37e4692d8a4224cf743
YuriiSmakota <yurii.smakota@gmail.com>
2023-10-26 15:16:32 +0300

    Modified Keyboards

--- keyboards/builder_keyboard.py
+++ keyboards/builder_keyboard.py
@@ -2,36 +2,35 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 
 
 def get_reply_keyboard():
-    keyboard_builder = ReplyKeyboardBuilder()
-    keyboard_builder.button(text="ONE")
-    keyboard_builder.button(text="TWO")
-    keyboard_builder.button(text="THREE")
-    keyboard_builder.button(text="FOUR")
-    keyboard_builder.button(text="FIVE")
-    keyboard_builder.button(text="SEX")
-    keyboard_builder.button(text="SEVEN")
-    keyboard_builder.button(text="EIGHT")
-    keyboard_builder.button(text="NINE")
-    keyboard_builder.button(text="TEN"),
+    builder = ReplyKeyboardBuilder()
+    builder.button(text="ONE")
+    builder.button(text="TWO")
+    builder.button(text="THREE")
+    builder.button(text="FOUR")
+    builder.button(text="FIVE")
+    builder.button(text="SEX")
+    builder.button(text="SEVEN")
+    builder.button(text="EIGHT")
+    builder.button(text="NINE")
+    builder.button(text="TEN"),
+    builder.adjust(1, 2, 3, 4)
 
-    keyboard_builder.adjust(1, 2, 3, 4),
-    return keyboard_builder.as_markup()
+    return builder.as_markup(resize_keyboard=True)
 
 
 def get_inline_keyboard():
-    keyboard_builder = InlineKeyboardBuilder()
-    keyboard_builder.button(text="1", callback_data="1")
-    keyboard_builder.button(text="2", callback_data="2")
-    keyboard_builder.button(text="3", callback_data="3")
-    keyboard_builder.button(text="4", callback_data="4")
-    keyboard_builder.button(text="5", callback_data="5")
-    keyboard_builder.button(text="6", callback_data="7")
-    keyboard_builder.button(text="7", callback_data="7")
-    keyboard_builder.button(text="8", callback_data="8")
-    keyboard_builder.button(text="9", callback_data="9")
-    keyboard_builder.button(text="10", callback_data="10")
+    builder = InlineKeyboardBuilder()
+    builder.button(text="1", callback_data="1")
+    builder.button(text="2", callback_data="2")
+    builder.button(text="3", callback_data="3")
+    builder.button(text="4", callback_data="4")
+    builder.button(text="5", callback_data="5")
+    builder.button(text="6", callback_data="7")
+    builder.button(text="7", callback_data="7")
+    builder.button(text="8", callback_data="8")
+    builder.button(text="9", callback_data="9")
+    builder.button(text="10", callback_data="10")
+    builder.adjust(1, 2, 3, 4)
+    return builder.as_markup()
 
-    keyboard_builder.adjust(1, 2, 3, 4)
-    return keyboard_builder.as_markup()
-
-
+# from keyboards.builder_keyboard import get_reply_keyboard
--- keyboards/inline_keyboard.py
+++ keyboards/inline_keyboard.py
@@ -3,16 +3,12 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 base_inline = InlineKeyboardMarkup(
     inline_keyboard=[
         [
-            InlineKeyboardButton(text="CallBack Data", callback_data="CallBack Data")
+            InlineKeyboardButton(text="CallBack Data", callback_data="CallBack Data"),
+            InlineKeyboardButton(text="Google", url="https://google.com"),
         ],
         [
-            InlineKeyboardButton(text="Google", url="https://google.com")
-        ],
-        [
-            InlineKeyboardButton(text="Switch Inline Query", switch_inline_query="Telegram")
-        ],
-        [
+            InlineKeyboardButton(text="Switch Inline Query", switch_inline_query="Telegram"),
             InlineKeyboardButton(text="Switch Inline Query Current Chat", switch_inline_query_current_chat="Telegram")
-        ],
+        ]
     ]
 )
